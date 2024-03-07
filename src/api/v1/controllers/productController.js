a516cf29e13a65b34811e543a205dec28e469bba
Gerardo <grd6@outlook.com>
2024-03-07 04:46:54 -0300

    Create productsControler

---
+++ src/api/v1/controllers/productController.js
@@ -0,0 +1,44 @@
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await getAllProducts();
+    res.status(200).json({ products: products });
+  } catch (error) {
+    console.log(error);
+    const errorFound = findError(error.code);
+    return res
+      .status(errorFound[0]?.status)
+      .json({ error: errorFound[0]?.message });
+  }
+};
+
+const updateProducts = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { products } = req.body;
+    const products_update = await updateProducts(id, products);
+    res.status(200).json({ products: products_update });
+  } catch (error) {
+    const errorFound = findError(error.code);
+    return res
+      .status(errorFound[0].status)
+      .json({ error: errorFound[0].message });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteProduct = await destroyProduct(id);
+    if (deleteProduct === 0) {
+      return res.status(404).json({ message: "No existe el registro" });
+    }
+    res.status(204).json({ message: "registro eliminado con exito" });
+  } catch (error) {
+    const errorFound = findError(error.code);
+    return res
+      .status(errorFound[0].status)
+      .json({ error: errorFound[0].message });
+  }
+};
+
+export { getAllProducts, updateProducts, deleteProduct };

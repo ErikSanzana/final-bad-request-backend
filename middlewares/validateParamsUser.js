6cb836a4a369f2a0aae5e7dcef8269487a90afe4
Ignacio Bustos <ark.zikael@gmail.com>
2024-03-09 20:12:23 -0300

    update

--- middlewares/validateParamsUser.js
+++ middlewares/validateParamsUser.js
@@ -1,6 +1,6 @@
 const validateParametersUser = (req, res, next) => {
     const { user } = req.body;
-    if (!user.email || !user.password || !user.nombre || !user.apellido) {
+    if (!user.email || !user.password || !user.rut || !user.name || !user.last_name|| !user.last_name_second|| !user.birth_date) {
         return res.status(400).json({ error: "el email, password, nombre y apellido deben estar presentes" });
     }
     next();

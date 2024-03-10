f100c5eb71bc55dac403cd35f47f762e4d2917d4
Gerardo <grd6@outlook.com>
2024-03-10 12:29:47 -0300

    Update userRoutesGrd.js

--- config/routes/userRoutesGrd.js
+++ config/routes/userRoutesGrd.js
@@ -1,14 +1,17 @@
 import express from "express";
 import {
-  addFavorites,
-  createNewUser,
-  likedFavorites,
-  updateUser,
+  
+    createNewUser
+ 
+
 } from "../../src/api/v1/controllers/usercontrollerGrd.js";
-import { validateParametersUserGrd } from "../../middlewares/validateParamsUserGrd.js";
+//import { validateParametersUserGrd } from "../../middlewares/validateParamsUserGrd.js";
 import { notFound } from "../../src/api/v1/controllers/notFoundController.js";
 
 const router = express.Router();
 
-router.post("/user", validateParametersUserGrd, createNewUser);
+//router.post("/user", validateParametersUserGrd, createNewUser);
 router.post("/user", createNewUser);
+
+
+export default router;

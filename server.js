4f0866c3ab932ed1e7d7153160b649110377de28
Ximena <32523009+ximenagp@users.noreply.github.com>
2024-03-10 22:43:48 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 import express from "express";
 import cors from "cors";
 import productRoutes from "./config/routes/productRoutes.js";
-// import { logger } from "logger-express";
+import { logger } from "logger-express";
 import loginRoutes from "./config/routes/loginRoutes.js";
 //import userRoutes from "./config/routes/userRoutes.js";
 //import travelsRouter from "./config/routes/travelRoutes.js";
 //GRD
-import userRoutesGrd from "./config/routes/userRoutes.js";
+import userRoutesGrd from "./config/routes/userRoutesGrd.js";
 // import userRoutes from "./config/routes/userRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+import{V1SwaggerDocs } from './v1/swagger'
 
-// swagger(app)
+// swagger
 app.options("*", cors());
 app.use(express.json());
 //grd
-// app.use(logger());
+app.use(logger());
 app.use("/api/v1", productRoutes);
 app.use("/api/v1", loginRoutes);
 // app.use("*", notFound); // para rutas inexistentes //tengo duda existencial si va aca o en las routes como lo deje ( o si en 1 de los archivos solamente)
@@ -33,6 +34,7 @@ app.listen(PORT, (error) => {
     console.log(` app listening at ⚡http://localhost:${PORT}⚡`);
     // console.log(`Swagger docs available at http://localhost:${PORT}/api/v1/docs`);
   }
+  V1SwaggerDocs(app, PORT)
 });
 
 export default app;
